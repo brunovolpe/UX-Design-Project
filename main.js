c8f7f41df266b4ad79f3bbe4e24f3a29e2bc9114
Asen K <krushkov1914@gmail.comgit$git config --global user.email krushkov1914@gmail.com>
2022-03-20 19:57:12 +0100

    Koge's map fixed
    need only MAPS to be put there

--- main.js
+++ main.js
@@ -66,15 +66,15 @@ function bedClick(){
     if(imgOn.length < 1){
         let popUpClasses = document.getElementsByClassName('popUpContainer');
         bedCreater();
-        bedCreater('1');
-        bedCreater('2');
-        bedCreater('3');
-        bedCreater('4');
-        bedCreater('5');
+        bedCreater('111');
+        bedCreater('112');
+        bedCreater('113');
+        bedCreater('114');
+        bedCreater('115');
         imgOn[0].addEventListener('click', function(){
             
             if(popUpClasses.length < 1){
-                popUpCreator('Danhostel','imgForPopUp','Vamdrupvej 1, 4600 Køge','www.danhostel-koege.dk','+45 61 18 12 03','3.8',' ')
+                popUpCreator('Danhostel','imgFirstBed','Vamdrupvej 1, 4600 Køge','www.danhostel-koege.dk','+45 61 18 12 03','3.8',' ')
                 popUpClasses[0].classList.add('firstBed')
 
             }else{
@@ -86,8 +86,8 @@ function bedClick(){
         imgOn[1].addEventListener('click', function(){
             
             if(popUpClasses.length < 1){
-                popUpCreator("Annette's Annex",'imgForPopUp','Pedersvej 22, 4600 Køge','www.visitkoege.dk','+45 60 84 14 30','4.6',' ')
-                popUpClasses[0].classList.add('firstBed')
+                popUpCreator("Annette's Annex",'imgForPopUp','Pedersvej 22, 4600 Køge','www.visitkoege.dk','+45 60 84 14 30','4.6','AIRBNB')
+                popUpClasses[0].classList.add('secBed')
 
             }else{
                 for(let i = popUpClasses.length ; i>0; i--){
@@ -98,8 +98,8 @@ function bedClick(){
         imgOn[2].addEventListener('click', function(){
             
             if(popUpClasses.length < 1){
-                popUpCreator('Danhostel','imgForPopUp','Vamdrupvej 1, 4600 Køge','www.danhostel-koege.dk','+45 61 18 12 03','3.8',' ')
-                popUpClasses[0].classList.add('firstBed')
+                popUpCreator('Centralhotellet','imgForPopUp','Vestergade 3, 4600 Køge','www.centralhotellet.dk','+45 56 65 06 96','4.2',' ');
+                popUpClasses[0].classList.add('tirthBed');
 
             }else{
                 for(let i = popUpClasses.length ; i>0; i--){
@@ -110,8 +110,8 @@ function bedClick(){
         imgOn[3].addEventListener('click', function(){
             
             if(popUpClasses.length < 1){
-                popUpCreator('Danhostel','imgForPopUp','Vamdrupvej 1, 4600 Køge','www.danhostel-koege.dk','+45 61 18 12 03','3.8',' ')
-                popUpClasses[0].classList.add('firstBed')
+                popUpCreator('Hotel Niels Juel','imgForPopUp','Toldbodvej 20, 4600 Køge','www.hotelnielsjuel.dk','+45 56 63 18 00','3.7',' ');
+                popUpClasses[0].classList.add('fourthBed');
 
             }else{
                 for(let i = popUpClasses.length ; i>0; i--){
@@ -122,8 +122,8 @@ function bedClick(){
         imgOn[4].addEventListener('click', function(){
             
             if(popUpClasses.length < 1){
-                popUpCreator('Danhostel','imgForPopUp','Vamdrupvej 1, 4600 Køge','www.danhostel-koege.dk','+45 61 18 12 03','3.8',' ')
-                popUpClasses[0].classList.add('firstBed')
+                popUpCreator('Comwell Køge Strand','imgForPopUp','Strandvejen 111, 4600 Køge','www.comwell.com','+45 56 65 36 90','4.1',' ')
+                popUpClasses[0].classList.add('fifthBed')
 
             }else{
                 for(let i = popUpClasses.length ; i>0; i--){
@@ -134,8 +134,8 @@ function bedClick(){
         imgOn[5].addEventListener('click', function(){
             
             if(popUpClasses.length < 1){
-                popUpCreator('Danhostel','imgForPopUp','Vamdrupvej 1, 4600 Køge','www.danhostel-koege.dk','+45 61 18 12 03','3.8',' ')
-                popUpClasses[0].classList.add('firstBed')
+                popUpCreator('Vallo Camping','imgForPopUp','Strandvejen 102, 4600 Køge','www.valloevamping.dk','+45 56 65 28 51','3.9',' ')
+                popUpClasses[0].classList.add('sixBed')
 
             }else{
                 for(let i = popUpClasses.length ; i>0; i--){
@@ -153,6 +153,201 @@ function bedClick(){
     
     
 }
+
+function EatDrinkClick(){
+    let imgOn = document.getElementsByClassName('iconContainer');
+
+    if(imgOn.length < 1){
+        let popUpClasses = document.getElementsByClassName('popUpContainer');
+        drinksCreater('220');
+        drinksCreater('221');
+        drinksCreater('222');
+        drinksCreater('223');
+        drinksCreater('224');
+        drinksCreater('225');
+
+    }else{
+        for(let i =imgOn.length; i>0; i--){
+            imgOn[i-1].remove();
+        }
+    }
+}
+function shoppingClick(){
+    let imgOn = document.getElementsByClassName('iconContainer');
+    if(imgOn.length <1){
+        let popUpClasses = document.getElementsByClassName('popUpContainer');
+        shoppingCreater('770');
+        shoppingCreater('771');
+        shoppingCreater('772');
+        shoppingCreater('773');
+        shoppingCreater('774');
+        shoppingCreater('775');
+        
+    }else{
+        for(let i =imgOn.length; i>0; i--){
+            imgOn[i-1].remove();
+        }
+    }
+}
+function harborClick(){
+        let imgOn = document.getElementsByClassName('iconContainer');
+        if(imgOn.length <1){
+            let popUpClasses = document.getElementsByClassName('popUpContainer');
+            shipsCreater('330');
+            shipsCreater('331');
+            shipsCreater('332');
+            shipsCreater('333');
+            shipsCreater('334');
+            imgOn[0].addEventListener('click', function(){
+            
+                if(popUpClasses.length < 1){
+                    popUpCreator('Danhostel','imgFirstBed','Revlen 1, 4600 Køge','www.visitkoege.dk','+45 60 84 14 30','3.8',' ')
+                    popUpClasses[0].classList.add('firstShip')
+    
+                }else{
+                    for(let i = popUpClasses.length ; i>0; i--){
+                        popUpClasses[i-1].remove();
+                    }
+                }
+            })
+            imgOn[1].addEventListener('click', function(){
+            
+                if(popUpClasses.length < 1){
+                    popUpCreator('Køge Svømmeland','imgFirstBed','Olbycenter 104, 4600 Køge','www.svoemmeland.dk','+45 56 67 27 70','4.5',' ')
+                    popUpClasses[0].classList.add('secShip')
+    
+                }else{
+                    for(let i = popUpClasses.length ; i>0; i--){
+                        popUpClasses[i-1].remove();
+                    }
+                }
+            })
+            imgOn[2].addEventListener('click', function(){
+            
+                if(popUpClasses.length < 1){
+                    popUpCreator('Køge Marina','imgFirstBed','Badehavnen 2, 4600 Køge','www.koegemarina.dk','+45 56 66 16 89','4.4',' ')
+                    popUpClasses[0].classList.add('tirthShip')
+    
+                }else{
+                    for(let i = popUpClasses.length ; i>0; i--){
+                        popUpClasses[i-1].remove();
+                    }
+                }
+            })
+            imgOn[3].addEventListener('click', function(){
+            
+                if(popUpClasses.length < 1){
+                    popUpCreator('Søndre Strand','imgFirstBed',' 4600 Køge','www.visitkoege.dk','','4.1',' ')
+                    popUpClasses[0].classList.add('forthShip')
+    
+                }else{
+                    for(let i = popUpClasses.length ; i>0; i--){
+                        popUpClasses[i-1].remove();
+                    }
+                }
+            })
+            imgOn[4].addEventListener('click', function(){
+            
+                if(popUpClasses.length < 1){
+                    popUpCreator('Strandvejen','imgFirstBed','Strandvejen, 4600 Køge','www.visitkoege.dk','','3.5',' ')
+                    popUpClasses[0].classList.add('fiveShip')
+    
+                }else{
+                    for(let i = popUpClasses.length ; i>0; i--){
+                        popUpClasses[i-1].remove();
+                    }
+                }
+            })
+
+        }else{
+            for(let i =imgOn.length; i>0; i--){
+                imgOn[i-1].remove();
+            }
+        }
+}
+function cultureClick(){
+    let imOn = document.getElementsByClassName('iconContainer');
+    if(imOn.length < 1){
+        let popUpClasses = document.getElementsByClassName('popUpContainer');
+        cultureCreater('440');
+    }else{
+        for(let i =imOn.length; i>0; i--){
+            imOn[i-1].remove();
+        }
+    }
+
+}
+function historyClick(){
+    let imOn = document.getElementsByClassName('iconContainer');
+    if(imOn.length < 1){
+        let popUpClasses = document.getElementsByClassName('popUpContainer');
+        historyCreater('550');
+    }else{
+        for(let i =imOn.length; i>0; i--){
+            imOn[i-1].remove();
+        }
+    }
+}
+function indoorClick(){
+    let imOn = document.getElementsByClassName('iconContainer');
+    if(imOn.length < 1){
+        let popUpClasses = document.getElementsByClassName('popUpContainer');
+        indoorCreater('660');
+    }else{
+        for(let i =imOn.length; i>0; i--){
+            imOn[i-1].remove();
+        }
+    }
+}
+function outdoorClick(){
+    let imgOn = document.getElementsByClassName('iconContainer');
+    if(imgOn.length < 1){
+        let popUpClasses = document.getElementsByClassName('popUpContainer');
+        outdoorCreater('880');
+        outdoorCreater('881');
+        outdoorCreater('882');
+        imgOn[0].addEventListener('click', function(){
+            
+            if(popUpClasses.length < 1){
+                popUpCreator('Koge River','imgFirstBed','Køge A, Køge Municipality','www.visitkoege.dk','','',' ')
+                popUpClasses[0].classList.add('firstOutdoor')
+
+            }else{
+                for(let i = popUpClasses.length ; i>0; i--){
+                    popUpClasses[i-1].remove();
+                }
+            }
+        })
+        imgOn[1].addEventListener('click', function(){
+            
+            if(popUpClasses.length < 1){
+                popUpCreator('Køge Kano og Kajak Klub','imgFirstBed','Søndre Molevej 20, Køge','www.koegekajak.dk','+45 56 63 42 52','4.7',' ')
+                popUpClasses[0].classList.add('secondOutdoor')
+
+            }else{
+                for(let i = popUpClasses.length ; i>0; i--){
+                    popUpClasses[i-1].remove();
+                }
+            }
+        })
+        imgOn[2].addEventListener('click', function(){
+            
+            if(popUpClasses.length < 1){
+                popUpCreator('Strandvejen','imgFirstBed','Strandvejen, Køge','www.visitkoege.dk','','3.5',' ')
+                popUpClasses[0].classList.add('tirthOutdoor')
+
+            }else{
+                for(let i = popUpClasses.length ; i>0; i--){
+                    popUpClasses[i-1].remove();
+                }
+            }
+        })
+    }else{
+        for(let i =imgOn.length; i>0; i--){
+            imgOn[i-1].remove();
+        }
+    }
+}
     
 //popUpCreator('Straedet Koge','imgForPopUp','Olbycenter 104, 4600 Koge','www.straedetkoge.dk','+45 50 93 86 27','4.5','Shopping street')
 function bedCreater(counter){
@@ -162,6 +357,90 @@ function bedCreater(counter){
     imgContainer.classList.add('iconContainer')
     imgContainer.classList.add(string);
     img.classList.add('iconBed');
+
+    imgContainer.appendChild(img);
+    document.body.appendChild(imgContainer)
+    return;
+}
+function drinksCreater(counter){
+    let imgContainer = document.createElement('div');
+    let img = document.createElement('div');
+    let string = 'iconContainer' + counter;
+    imgContainer.classList.add('iconContainer')
+    imgContainer.classList.add(string);
+    img.classList.add('iconDrink');
+
+    imgContainer.appendChild(img);
+    document.body.appendChild(imgContainer)
+    return;
+}
+function shipsCreater(counter){
+    let imgContainer = document.createElement('div');
+    let img = document.createElement('div');
+    let string = 'iconContainer' + counter;
+    imgContainer.classList.add('iconContainer')
+    imgContainer.classList.add(string);
+    img.classList.add('iconShip');
+
+    imgContainer.appendChild(img);
+    document.body.appendChild(imgContainer)
+    return;
+}
+function cultureCreater(counter){
+    let imgContainer = document.createElement('div');
+    let img = document.createElement('div');
+    let string = 'iconContainer' + counter;
+    imgContainer.classList.add('iconContainer')
+    imgContainer.classList.add(string);
+    img.classList.add('iconCulture');
+
+    imgContainer.appendChild(img);
+    document.body.appendChild(imgContainer)
+    return;
+}
+function historyCreater(counter){
+    let imgContainer = document.createElement('div');
+    let img = document.createElement('div');
+    let string = 'iconContainer' + counter;
+    imgContainer.classList.add('iconContainer')
+    imgContainer.classList.add(string);
+    img.classList.add('iconHistory');
+
+    imgContainer.appendChild(img);
+    document.body.appendChild(imgContainer)
+    return;
+}
+function indoorCreater(counter){
+    let imgContainer = document.createElement('div');
+    let img = document.createElement('div');
+    let string = 'iconContainer' + counter;
+    imgContainer.classList.add('iconContainer')
+    imgContainer.classList.add(string);
+    img.classList.add('iconIndoor');
+
+    imgContainer.appendChild(img);
+    document.body.appendChild(imgContainer)
+    return;
+}
+function outdoorCreater(counter){
+    let imgContainer = document.createElement('div');
+    let img = document.createElement('div');
+    let string = 'iconContainer' + counter;
+    imgContainer.classList.add('iconContainer')
+    imgContainer.classList.add(string);
+    img.classList.add('iconOutDoor');
+
+    imgContainer.appendChild(img);
+    document.body.appendChild(imgContainer)
+    return;
+}
+function shoppingCreater(counter){
+    let imgContainer = document.createElement('div');
+    let img = document.createElement('div');
+    let string = 'iconContainer' + counter;
+    imgContainer.classList.add('iconContainer')
+    imgContainer.classList.add(string);
+    img.classList.add('iconShopping');
 
     imgContainer.appendChild(img);
     document.body.appendChild(imgContainer)
